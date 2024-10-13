2ab56c04fb1b8d452e0fcdb4d12ee85a2397f945
Andrew <diana.lezhava@gmail.com>
2024-10-13 11:54:30 +0400

    i uploaded them

---
+++ lvl 2 hw huge castle/the huge castle/The huge castle.py
@@ -0,0 +1,43 @@
+from turtle import *
+# i want to draw a castle
+
+# i want to draw a square first
+speed(1000000000000000000000000000000000000000000000000000000000000000000)
+width(8)
+color("yellow")
+forward(300)
+left(90)
+
+forward(300)
+left(90)
+
+forward(300)
+left(90)
+
+forward(300)
+left(90)
+
+forward(100)
+left(90)
+# i want to draw a door
+forward(150)
+right(90)
+
+forward(75)
+right(90)
+
+forward(150)
+left(90)
+
+forward(125)
+left(90)
+
+forward(300)
+left(70)
+
+forward(300)
+left(90)
+
+forward(150)
+
+exitonclick()
---
+++ python course/day1/my codes.py
@@ -0,0 +1,45 @@
+print("ValueError")
+
+
+print("https://www.facebook.com/share/ZzGcc27AqsEeTf7f/")
+
+print("Andrew killasonia")
+
+print("EnvironmentError")
+print("PS C:\Users\Andrew killasonia@ kilasa_>erorr ez\OneDrive\Desktop\python course\day1> & C:/Users/beqa_/AppData/Local/Programs/Python/Python313/python.exe")
+print("IalreadyDid5This6thOne")
+print(I am 5+6 years old)
+print("hello tree")
+print(20)
+print("Ihave2Sisters")
+print("TeachersWhenAreWeGonnaLearnjava")
+print("
+    *values: object,
+    sep: str | None = " ",
+    end: str | None = "\n",
+    file: SupportsWrite[str] | None = None,
+    flush: Literal[False] = False
+) -> None: ...
+
+def print(
+    *values: object,
+    sep: str | None = " ",
+    end: str | None = "\n",
+    file: _SupportsWriteAndFlush[str] | None = None,
+    flush: bool
+) -> None: ...
+Prints the values to a stream, or to sys.stdout by default.
+
+sep
+  string inserted between values, default a space.
+end
+  string appended after the last value, default a newline.
+file
+  a file-like object (stream); defaults to the current sys.stdout.
+flush
+  whether to forcibly flush the stream")
+print("stream")
+print("GioIsMyUncle")
+
+
+
