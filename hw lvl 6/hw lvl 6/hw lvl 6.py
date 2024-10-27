f2a16864fc39d7c0502cc61e792891ad821f8520
Andrew <diana.lezhava@gmail.com>
2024-10-27 14:58:37 +0400

    Add files via upload

---
+++ hw lvl 6/hw lvl 6/hw lvl 6.py
@@ -0,0 +1,26 @@
+me = 31
+print(me)
+mom = 55
+print(mom)
+dad = 57
+print(dad)
+sis = 27
+print(sis)
+lil sis = 20
+print(lil sis)
+aunt = 58
+print(aunt)
+uncle1 = 53
+print(uncle1)
+uncle2 = 60
+print(uncle2)
+grandma1 = 87
+print(grandma1)
+grandma2 = 75
+print(grandma2)
+dog1 = 22
+print(dog1)
+dog2 = 25
+print(dog2)
+parrot = 21
+print(parrot)
